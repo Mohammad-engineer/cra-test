93590165312289c6ae82e2449009416a3b695713
m.mohammadi <mohammad7m8@gmail.com>
2023-09-09 14:59:20 +0330

    commit2

---
+++ src/components/videos.jsx
@@ -0,0 +1,12 @@
+import React ,{useState}from 'react'
+
+const videos = () => {
+    constp [videos,setVideos] = useState(null)
+  return (
+    <div>videos
+    
+    </div>
+  )
+}
+
+export default videos
